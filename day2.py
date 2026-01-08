3198e089103feab67c8cb629353d0aab24e372ef
Swapnarani panda <swapnapanda908@gmail.com>
2026-01-08 20:00:55 +0530

    init

---
+++ day2.py
@@ -0,0 +1,114 @@
+#sequence types
+#string
+#it's immutable datatype:the iteams can't be assignment and can't be update
+# c="swapna"
+# b="tunia"
+# print(c)
+# print(c[4])
+# print(type(c))
+# print(c+b)
+# # c[4]=a donot support any assignments
+# print(c)
+# for c in range(1,5):
+#   print(c)
+c="tunia"
+for b in c:
+  print(b)
+
+#list or array
+#it is mutable data type 
+# lst=[1,2,3,"swapna"]
+# print(lst)
+# print(lst[1])
+# print(type(lst))
+# lst[2]='4'
+# print(lst)
+# str=lst.remove(1)
+# print(str)
+# print(lst)
+
+
+#tuple
+#immutable datatype
+# tup=(1,2,3,4)
+# print(tup)
+# print(type(tup)) 
+# # tup[3]=6 // not possible beacause it is immutable datatype
+# print(tup)
+
+
+#set :unique values only write no duplicate value store
+#immutable datatype
+# set={1,2,2,3,4,5,"swapna"}
+# print(set)
+# print(type(set))
+# set[2]=5
+# print(set)
+# print(set[1]) //set doesn,t supporting indexing
+
+#dictionary  ,mapping type :reference create
+# dict={
+#   'name':'swapna',
+#   'age':'21',
+#   'regd':'113',
+#   'course':['math','science']
+# }
+# print(dict)
+# print(dict['name'])
+# print(dict ['course'][1])
+# print(type(dict))
+# dict['name']='tunia'
+# print(dict)
+
+# dict1={
+#   'name':"tunia",
+#   'age':21,
+#   "adress":{
+#     'village':'kalahandia',
+#     'dist':'bhadrak',
+#     'via':'barikpur bazar',
+#     'pin':'756112',
+#    } }
+# print(dict1['adress']['village'])
+# print(type(dict1))
+
+# #None type
+# count=None
+# print(count)
+# print(type(count))
+
+# # operater:it  is a symbol or a keywords that is used for specific tasks
+
+# #operater
+# a=10
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# # print(a//b) :double divider means float to integer valu
+
+# #assignment operater
+
+# b=15
+# b+=2
+# b*=9
+# b//=2
+# b%=4
+
+# print(b)
+
+# a=30
+# print(a)
+# a+=2
+# print(a)
+# a//=2
+# print(a)
+# a//=2
+# print(a)
+# a%=4
+# print(a)
+
+
